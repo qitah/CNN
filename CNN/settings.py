d7862a825bb86ae84cf4e35991b7b6a2dee3552e
Abdullah <abdullah.qitah@gmail.com>
2022-05-06 11:30:58 +0300

    a

--- CNN/settings.py
+++ CNN/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ahlx*a($##0+i3)e%f!r($)zhphef+--6$8*xyg6%#!g@es$qh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
